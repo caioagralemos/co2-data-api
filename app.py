7a1fcd5825cb9ad3b345a48de73eebc47fd8849c
Caio <caioagralemos@live.com>
2024-10-04 02:36:14 -0300

    api

--- app.py
+++ app.py
@@ -1,6 +1,86 @@
-from flask import Flask
+import requests
+
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['POST'])
+def processar_coordenadas():
+  try:
+    data = request.get_json()
+    latitude = float(data['latitude'])
+    longitude = float(data['longitude'])
+
+    if not (-90 <= latitude <= 90) or not (-180 <= longitude <= 180):
+      return jsonify({'error': 'Invalid coordinates'}), 400
+
+    result = getData(latitude, longitude)
+
+    return jsonify({'result': result})
+
+  except requests.exceptions.RequestException as e:
+    return jsonify({'error': f'HTTP req error: {e}'}), 500
+  except KeyError as e:
+    return jsonify({'error': f'Key error: {e}'}), 500
+  except ValueError as e:
+    return jsonify({'error': f'Value error: {e}'}), 500
+  except Exception as e:
+    return jsonify({'error': str(e)}), 500
+
+
+def getData(latitude, longitude):
+    STAC_API_URL = "https://earth.gov/ghgcenter/api/stac"
+    RASTER_API_URL = "https://earth.gov/ghgcenter/api/raster"
+    collection_name = "odiac-ffco2-monthgrid-v2023"
+
+    items = requests.get(
+        f"{STAC_API_URL}/collections/{collection_name}/items?limit=300"
+    ).json()["features"]
+
+    local = {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+            "coordinates": [
+                [
+                    [longitude, latitude],
+                    [longitude, latitude-0.20],
+                    [longitude-0.20, latitude-0.20],
+                    [longitude-0.20, latitude],
+                    [longitude, latitude],
+                ]
+            ],
+            "type": "Polygon",
+        },
+    }
+
+    asset_name = "co2-emissions"
+
+
+    def generate_stats(item, geojson):
+        result = requests.post(
+            f"{RASTER_API_URL}/cog/statistics",
+            params={"url": item["assets"][asset_name]["href"]},
+            json=geojson,
+        ).json()
+
+        return {
+            **result["properties"],
+            "start_datetime": item["properties"]["start_datetime"][:7],
+        }
+
+
+    stats = generate_stats(items[0], local)
+
+    co2data = stats['statistics']['b1']
+    return f"CO2 data (monthly emissions on tonne C/km²/month):\n     minimal: {co2data['min']}\n     mean {co2data['mean']}\n     max {co2data['max']}\n     majority {co2data['majority']}"
+
+if __name__ == '__main__':
+  app.run(debug=True)
+
+@app.route('/', methods=['GET'])
+def info():
+  return jsonify({'message': 'Send a latitude/longitude post request to get CO2 emissions data.'})
+
+if __name__ == '__main__':
+  app.run(debug=True)
